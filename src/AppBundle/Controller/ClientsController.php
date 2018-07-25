6092f79760d6df777303c1bed4818f2a4f95ef6d
Eldar <eldar@kaveret.biz>
2018-07-25 09:06:02 +0300

    Use views

--- src/AppBundle/Controller/ClientsController.php
+++ src/AppBundle/Controller/ClientsController.php
@@ -10,6 +10,40 @@ use Symfony\Component\HttpFoundation\Request;
 class ClientsController extends Controller
 {
 
+private $client_data = [
+        [   'id' => 0 ,
+            'title' => 'mr',
+            'name' => 'Roy',
+            'last_name' => 'Adams',
+            'address' => '2872 Marquette Street',
+            'zip_code' => '10312',
+            'city' => 'New York City',
+            'state' => 'NY',
+            'email' => 'radams1v@example.com'
+        ],
+        [   'id' => 1 ,
+            'title' => 'mrs',
+            'name' => 'Bonnie',
+            'last_name' => 'Clark',
+            'address' => '4 Porter Avenue',
+            'zip_code' => '80028',
+            'city' => 'Louisville',
+            'state' => 'CO',
+            'email' => 'bclark6@example.com'
+        ],
+        [   'id' => 2 ,
+            'title' => 'ms',
+            'name' => 'Carol',
+            'last_name' => 'Shaw',
+            'address' => '650 Grover Alley',
+            'zip_code' => '30305',
+            'city' => 'Atlanta',
+            'state' => 'GA',
+            'email' => 'cshaw@example.com'
+         ]
+    ];
+
+    private $titles = ['mr', 'ms', 'mrs', 'dr', 'mx'];
 
     /**
      * @Route("/guests", name="index_clients")
@@ -17,7 +51,9 @@ class ClientsController extends Controller
     public function showIndex()
     {
 
-        return $this->render("clients/index.html.twig");
+        $data = [];
+        $data['clients'] = $this->client_data;
+        return $this->render("clients/index.html.twig", $data);
 
     }
 
